61c535639e689a52c88ff3f15d2fcd4a9fe37068
ahmed <bos-violation@stc.com.sa>
2021-01-09 05:23:21 +0300

    edit callback

--- app.py
+++ app.py
@@ -65,28 +65,15 @@ def create_app(test_config=None):
 
   @app.route('/callback', methods=['GET'])
   def decode_jwt():
-     """
-      Check user token and return non-secret data
-     """
-     if not 'Authorization' in request.headers:
-        abort(401)
-     data = request.headers['Authorization']
-
-     token = str.replace(str(data), 'Bearer ', '')
-     try:
-          data = jwt.decode(token, SECRET, algorithms=['HS256'])
-     except:
-          abort(401)
-     response = jsonify(token=_get_jwt(data).decode('utf-8'))
-     return jsonify(**response)
-
-
-  def _get_jwt(user_data):
-        exp_time = datetime.datetime.utcnow() + datetime.timedelta(weeks=2)
-        payload = {'exp': exp_time,
-               'nbf': datetime.datetime.utcnow(),
-               'email': user_data['email']}
-        return jwt.encode(payload, SECRET, algorithm='HS256')
+    # Handles response from token endpoint
+    auth0.authorize_access_token()
+    resp = auth0.get('userinfo')
+    userinfo = resp.json()
+    # Store the user information in flask session.
+    session['jwt_payload'] = userinfo
+    return jsonify({
+        'jwt' : userinfo
+    })
 
   @app.route('/login')
   def login():
